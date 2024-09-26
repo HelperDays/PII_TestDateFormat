ba6f0d55939b9a4a1c23c4365d06d620b813a9e6
Ale Hernandez <alehernandez@gmail.com>
2024-09-26 19:16:45 -0300

    Sln creado

---
+++ Tests/LibraryTests/DateFormatterTests.cs
@@ -0,0 +1,17 @@
+namespace LibraryTests;
+using NUnit.Framework;
+
+public class DateFormatterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+    
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
